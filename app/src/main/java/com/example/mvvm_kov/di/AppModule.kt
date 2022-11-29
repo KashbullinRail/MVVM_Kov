15e34ef858ab6ece3cac2499db863dccaf16eed1
KashbullinRail <kashbullinrail@gmail.com>
2022-11-29 11:08:15 +0300

    завершен код в AppModule.kt

--- app/src/main/java/com/example/mvvm_kov/di/AppModule.kt
+++ app/src/main/java/com/example/mvvm_kov/di/AppModule.kt
@@ -1,11 +1,27 @@
 package com.example.mvvm_kov.di
 
+import android.content.Context
+import com.example.mvvm_kov.domain.usecase.GetUserNameUseCase
+import com.example.mvvm_kov.domain.usecase.SaveUserNameUseCase
+import com.example.mvvm_kov.presentation.MainViewModelFactory
 import dagger.Module
 
 
 @Module
-class AppModule {
+class AppModule(val context: Context) {
 
+    fun provideContext(): Context {
+        return context
+    }
 
+    fun provideMainViewModelFactory(
+        getUserNameUseCase: GetUserNameUseCase,
+        saveUserNameUseCase: SaveUserNameUseCase
+    ): MainViewModelFactory {
+        return MainViewModelFactory(
+            getUserNameUseCase = getUserNameUseCase,
+            saveUserNameUseCase = saveUserNameUseCase
+        )
+    }
 
 }
