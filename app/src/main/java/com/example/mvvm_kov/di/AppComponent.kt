1f0e13a847fe376e41cca3727ce4756af9c8f028
KashbullinRail <kashbullinrail@gmail.com>
2022-11-29 12:13:16 +0300

    переход на dagger завершен

--- app/src/main/java/com/example/mvvm_kov/di/AppComponent.kt
+++ app/src/main/java/com/example/mvvm_kov/di/AppComponent.kt
@@ -6,5 +6,7 @@ import dagger.Component
 
 @Component(modules = [AppModule::class, DomainModule::class, DataModule::class])
 interface AppComponent {
+
     fun inject(mainActivity: MainActivity)
+
 }
