4c224cfd19ad95e94b044614381fe30ca143bbce
KashbullinRail <kashbullinrail@gmail.com>
2022-12-02 12:42:56 +0300

    написаны тесты для GetUserNameUseCase.kt с использованием библиотеки Mockito

--- domain/src/test/java/com/example/mvvm_kov/domain/usecase/GetUserNameUseCaseTest.kt
+++ domain/src/test/java/com/example/mvvm_kov/domain/usecase/GetUserNameUseCaseTest.kt
@@ -5,29 +5,35 @@ import com.example.mvvm_kov.domain.models.UserName
 import com.example.mvvm_kov.domain.repository.UserRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.mock
 
 
-class TestRepository: UserRepository {
-    override fun saveName(saveUserName: SaveUserName): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun getName(): UserName {
-        return UserName(firstName = "test first name", lastName = "test last name")
-    }
-
-}
+//class TestRepository: UserRepository {
+//    override fun saveName(saveUserName: SaveUserName): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getName(): UserName {
+//        return UserName(firstName = "test first name", lastName = "test last name")
+//    }
+//
+//}
 
 
 class GetUserNameUseCaseTest {
 
+    val userRepository = mock<UserRepository>()
 
     @Test
     fun `sould return the same data as in repository`() {
 
-        val testRepository = TestRepository()
+        val testUserName = UserName(firstName = "test first name", lastName = "test last name")
+       Mockito.`when`(userRepository.getName()).thenReturn(testUserName)
 
-        val useCase = GetUserNameUseCase(userRepository = testRepository)
+        val useCase = GetUserNameUseCase(userRepository = userRepository)
         val actual = useCase.execute()
         val expected = UserName(firstName = "test first name", lastName = "test last name")
 
