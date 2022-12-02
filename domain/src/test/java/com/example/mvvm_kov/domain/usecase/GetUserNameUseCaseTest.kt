d3e6e4b187550e3fc28e4cdf89fccc4ca5b144a4
KashbullinRail <kashbullinrail@gmail.com>
2022-12-02 11:07:23 +0300

    написаны тесты для GetUserNameUseCase.kt вручную

--- domain/src/test/java/com/example/mvvm_kov/domain/usecase/GetUserNameUseCaseTest.kt
+++ domain/src/test/java/com/example/mvvm_kov/domain/usecase/GetUserNameUseCaseTest.kt
@@ -1,13 +1,38 @@
 package com.example.mvvm_kov.domain.usecase
 
+import com.example.mvvm_kov.domain.models.SaveUserName
+import com.example.mvvm_kov.domain.models.UserName
+import com.example.mvvm_kov.domain.repository.UserRepository
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+
+
+class TestRepository: UserRepository {
+    override fun saveName(saveUserName: SaveUserName): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getName(): UserName {
+        return UserName(firstName = "test first name", lastName = "test last name")
+    }
+
+}
+
 
 class GetUserNameUseCaseTest {
 
 
-
     @Test
-    fun test1(){
+    fun `sould return the same data as in repository`() {
+
+        val testRepository = TestRepository()
+
+        val useCase = GetUserNameUseCase(userRepository = testRepository)
+        val actual = useCase.execute()
+        val expected = UserName(firstName = "test first name", lastName = "test last name")
+
+        Assertions.assertEquals(expected, actual)
+
 
     }
 
